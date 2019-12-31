955036e5a4155bcba9ac20f6c7257ffda69ff67f
PundoJnr <pundoarthur@gmail.com>
2019-12-31 22:15:30 +0300

    Add meal time

--- app/src/main/java/com/pundo/everything/MainActivity.kt
+++ app/src/main/java/com/pundo/everything/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.pundo.everything
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -8,14 +9,25 @@ class MainActivity : AppCompatActivity() {
 
 
     val foodList = arrayListOf("beef and kales", "chicken and cabbage", "milk")
+    var meal = arrayOf("Select meal time", "Breakfast", "Lunch", "Diner")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //init
+        spinner!!.setOnItemListener(this)
+
+
+        //Create an Array adapter
+        val mealType = ArrayAdapter(this, android.R.layout.simple_spinner_item, meal)
+        //set layout for spinner
+        mealType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        //set Adapter
+        spinner!!.setAdapter(mealType)
+
 
         decide.setOnClickListener {
             val random = java.util.Random()
-
             val randomFood = random.nextInt(foodList.count())
             decidedMeal.text = foodList[randomFood];
         }

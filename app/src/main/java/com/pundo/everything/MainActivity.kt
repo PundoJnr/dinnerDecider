72e9058f25ae1427f2dd5d1608781a81fc5f8056
PundoJnr <pundoarthur@gmail.com>
2019-12-31 20:29:50 +0300

    final commit

--- app/src/main/java/com/pundo/everything/MainActivity.kt
+++ app/src/main/java/com/pundo/everything/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.pundo.everything
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val TIME_OUT = 3000;
+
+    val foodList = arrayListOf("beef and kales", "chicken and cabbage", "milk")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        decide.setOnClickListener {
+            val random = java.util.Random()
+
+            val randomFood = random.nextInt(foodList.count())
+            decidedMeal.text = foodList[randomFood];
+        }
+
+        add.setOnClickListener {
+            val newFood = meal.text.toString()
+
+            foodList.add(newFood)
+            meal.text.clear()
+        }
 
 
     }

8a8730fc1c16158dcb6fc04b577399ea8a32277a
PundoJnr <pundoarthur@gmail.com>
2019-12-31 16:07:39 +0300

    Initial commit

---
+++ app/src/main/java/com/pundo/everything/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.pundo.everything
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    val TIME_OUT = 3000;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}

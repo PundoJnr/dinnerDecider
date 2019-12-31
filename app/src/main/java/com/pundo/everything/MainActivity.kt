a8588ef4da5e40c9ee28b93e000452e030809b71
PundoJnr <pundoarthur@gmail.com>
2019-12-31 22:33:16 +0300

    Add spinner

--- app/src/main/java/com/pundo/everything/MainActivity.kt
+++ app/src/main/java/com/pundo/everything/MainActivity.kt
@@ -1,29 +1,34 @@
 package com.pundo.everything
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity() , AdapterView.OnItemSelectedListener{
 
     val foodList = arrayListOf("beef and kales", "chicken and cabbage", "milk")
-    var meal = arrayOf("Select meal time", "Breakfast", "Lunch", "Diner")
+    var meals = arrayOf("Select meal time", "Breakfast", "Lunch", "Diner")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //init
-        spinner!!.setOnItemListener(this)
+
+
 
 
         //Create an Array adapter
-        val mealType = ArrayAdapter(this, android.R.layout.simple_spinner_item, meal)
+        val mealType = ArrayAdapter(this, android.R.layout.simple_spinner_item, meals)
         //set layout for spinner
         mealType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         //set Adapter
-        spinner!!.setAdapter(mealType)
+
+
+        fun onItemSelected(arg0: AdapterView<*>, arg1: View, position: Int, id: Long) {
+            // use position to know the selected item
+        }
 
 
         decide.setOnClickListener {
@@ -40,5 +45,14 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        when(position){
+
+        }
+    }
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }

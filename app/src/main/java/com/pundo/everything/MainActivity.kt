89f1db69bb8505e9dce18f6a5085a8f7991bef8f
PundoJnr <pundoarthur@gmail.com>
2020-01-02 10:01:46 +0300

    Add when

--- app/src/main/java/com/pundo/everything/MainActivity.kt
+++ app/src/main/java/com/pundo/everything/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.pundo.everything
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() , AdapterView.OnItemSelectedListener{
 
-    val foodList = arrayListOf("beef and kales", "chicken and cabbage", "milk")
+    val breakFast = arrayListOf("beef and kales", "chicken and cabbage", "milk")
     var meals = arrayOf("Select meal time", "Breakfast", "Lunch", "Diner")
+    val lunchList = arrayListOf("")
+    val dinnerList = arrayListOf("")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,25 +37,26 @@ class MainActivity : AppCompatActivity() , AdapterView.OnItemSelectedListener{
         }
 
 
-        decide.setOnClickListener {
-            val random = java.util.Random()
-            val randomFood = random.nextInt(foodList.count())
-            decidedMeal.text = foodList[randomFood];
-        }
-
-        add.setOnClickListener {
-            val newFood = meal.text.toString()
-
-            foodList.add(newFood)
-            meal.text.clear()
-        }
-
-
     }
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         when(position){
-
+            1 -> {
+                decide.setOnClickListener {
+                    val random = Random()
+                    val breakFastList = random.nextInt(breakFast.count())
+                    decidedMeal.text = breakFast[breakFastList]
+                }
+                add.setOnClickListener{
+                    //get user entry
+                    val newBreak = meal.text.toString()
+                    //add entry to arrayList
+                    breakFast.add(newBreak)
+                    //clear editText
+                    meal.text.clear()
+                }
+            }
+            else -> Toast.makeText(this@MainActivity,"aye",Toast.LENGTH_LONG).show()
         }
     }
     override fun onNothingSelected(parent: AdapterView<*>?) {
